86824e34a6b6d694eda9a237647f65ce656b7fa8
Sejin Kim <40668167+skim2257@users.noreply.github.com>
2022-11-07 23:41:32 +0000

    oui oui

--- convlassonet_synthetic.py
+++ convlassonet_synthetic.py
@@ -323,197 +323,195 @@ for sparse_me in ['conv2']:
     # print(images.shape, labels.shape)
 
     ### LassoNet
-    # for M in [1, 2]:
-        # try:
-    M = 1.
-    # for l1 in l1_range:
-    #     try:
-            
+    for M in [1, 2]:
+        try:
+    # M = 1.
+            for l1 in l1_range:
+                try:
+                    try:
+                        os.makedirs(plot_dir)
+                    except:
+                        pass
 
-    try:
-        os.makedirs(plot_dir)
-    except:
-        pass
+                    ### Random
+                    model_ = ConvLassoNet(lambda_=0., M=M, D_in=(28, 28), D_out=1, kernel_size=3, padding=0, sparse_layer=sparse_me).to(dev)
+                    try:
+                        os.makedirs(f'{plot_dir}/random')
+                    except:
+                        pass
+                    
+                    print(model_)
+                    show_layer(model_, 0, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=16)
+                    show_layer(model_, 3, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=32)
+                    show_layer(model_, 6, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=64)
+                    show_layer(model_, 9, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=64)
 
-    ### Random
-    model_ = ConvLassoNet(lambda_=0., M=M, D_in=(28, 28), D_out=1, kernel_size=3, padding=0, sparse_layer=sparse_me).to(dev)
-    try:
-        os.makedirs(f'{plot_dir}/random')
-    except:
-        pass
+                    # print(model_)
+                    # test forward method, reshape input to vector with view
+                    # model_.forward(images).size()
+                    # params of G are already included in params of model!
+                    for param in model_.parameters():
+                        print(param.size())
+
+
+
+                    loss_plots, acc_plots, auroc_plots = {}, {}, {}
+
+                    ### Unconstrained "init"
+                    opt_ = torch.optim.SGD(model_.parameters(), lr=alpha0, momentum=momentum, nesterov=True)
+                    sched_ = StepLR(opt_, step_size=1, gamma=0.9)
+                    # record first n_epochs of ramp up
+                    hist_ = train_model(model_, opt_, loss, lr_schedule=sched_, n_epochs=n_epochs, device=dev)
+                    model0 = deepcopy(model_)
+                    try:
+                        os.makedirs(f'{plot_dir}/init')
+                    except:
+                        pass
+                    
+
+                    show_layer(model_, 0, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=16)
+                    show_layer(model_, 3, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=32)
+                    show_layer(model_, 6, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=64)
+                    show_layer(model_, 9, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=64)
+
+
+                    loss_plots[0.]   = hist_['valid_loss']
+                    acc_plots[0.]    = hist_['valid_acc']
+                    auroc_plots[0.]  = hist_['valid_auroc']
+
+
+
+                    model = ConvLassoNet(lambda_=l1, M=M, D_in=(28, 28), D_out=1, kernel_size=3, padding=0, final='softmax', sparse_layer=sparse_me).to(dev)
+
+                    opt = torch.optim.SGD(model.parameters(), lr=alpha0, momentum=momentum, nesterov=True)
+                    sched = StepLR(opt, step_size=1, gamma=0.9)
+                    
+                    hist = train_model(model, opt, loss, lr_schedule=sched, n_epochs=n_epochs, pretrained=model_, device=dev)
+                    
+                    loss_plots[l1]  = hist_['valid_loss']
+                    acc_plots[l1]   = hist_['valid_acc']
+                    auroc_plots[l1] = hist_['valid_auroc']
+                    
+
+                    v_, v2_ = 1., 0.1
+                    """ Let the Plotting Begin !"""
+                    try:
+                        os.makedirs(f'{plot_dir}/{M:.1f}_{l1:.2f}')
+                        print(f"plotting {plot_dir}, {M:.1f}, {l1:.2f}")
+                    except:
+                        pass
+
+                    ### unconstrained weights
+                    fig, axs = plt.subplots(4,4)
+                    plot_filter1(model0.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v_, vmax=v_)
+                    fig.suptitle('Filter1 weights unconstrained')
+                    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter1_unc.png', dpi = 400)
+
+                    ### unconstrained weights
+                    fig, axs = plt.subplots(4,8)
+                    plot_filter2(model0.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v2_, vmax=v2_)
+                    fig.suptitle('Filter2 weights unconstrained')
+                    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter2_unc.png', dpi = 400)
+
+                    # Loss plot
+                    fig, axs = plt.subplots(1,3, figsize=(20, 8))
+
+                    ax = axs[0]
+                    ax.plot(hist['train_loss'], c = '#002635', marker = 'o', label = 'Train loss ConvLassoNet')
+                    ax.plot(hist['valid_loss'], c = '#002635', marker = 'x', ls = '--', label = 'Validation loss ConvLassoNet')
+
+                    ax.plot(hist_['train_loss'], c = '#AB1A25', marker = 'o', label = 'Train loss unconstrained')
+                    ax.plot(hist_['valid_loss'], c = '#AB1A25', marker = 'x', ls = '--', label = 'Validation loss unconstrained')
+
+                    ax.set_xlabel('Epoch')
+                    ax.set_ylim(0,)
+                    ax.legend()
+
+                    ax = axs[1]
+                    ax.plot(hist['train_acc'], c = '#002635', marker = 'o', label = 'Train accuracy ConvLassoNet', alpha=0.7, markersize=4.)
+                    ax.plot(hist['valid_acc'], c = '#002635', marker = 'x', ls = '--', label = 'Validation accuracy ConvLassoNet', markersize=4.)
+                    
+                    ax.plot(hist_['train_acc'], c = '#AB1A25', marker = 'o', label = 'Train accuracy unconstrained', alpha=0.7, markersize=2.)
+                    ax.plot(hist_['valid_acc'], c = '#AB1A25', marker = 'x', ls = '--', label = 'Validation accuracy unconstrained', markersize=2.)
+
+                    ax.set_xlabel('Epoch')
+                    ax.set_ylim(0.,1.)
+                    ax.legend()
+
+                    ax = axs[2]
+                    ax.plot(hist['train_auroc'], c = '#002635', marker = 'o', label = 'Train AUROC ConvLassoNet', alpha=0.7, markersize=4.)
+                    ax.plot(hist['valid_auroc'], c = '#002635', marker = 'x', ls = '--', label = 'Validation AUROC ConvLassoNet', markersize=4.)
+                    
+                    ax.plot(hist_['train_auroc'], c = '#AB1A25', marker = 'o', label = 'Train AUROC unconstrained', alpha=0.7, markersize=2.)
+                    ax.plot(hist_['valid_auroc'], c = '#AB1A25', marker = 'x', ls = '--', label = 'Validation AUROC unconstrained', markersize=2.)
+
+                    ax.set_xlabel('Epoch')
+                    ax.set_ylim(0.,1.)
+                    ax.legend()
+
+                    fig.suptitle('Loss with and without LassoNet constraint')
+
+                    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_loss.png', dpi = 400)
+
+                    fig, ax = plt.subplots()
+                    plot_filter_norm(model.to('cpu'), ax, color = '#002635')
+                    plot_filter_norm(model0.to('cpu'), ax, color = '#AB1A25')
+
+                    ax.set_ylabel('Max-norm of each filter channel')
+                    ax.set_xlabel('Conv1 output channel')
+                    ax.set_ylim(0,)
+
+                    p1 = mpatches.Patch(color='#002635', label='ConvLassoNet')
+                    p2 = mpatches.Patch(color='#AB1A25', label='unconstrained')
+                    ax.legend(handles=[p1,p2])
+
+                    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv1_filter_norm.png', dpi = 400)
+
+                    fig, ax = plt.subplots()
+                    plot_filter_norm(model.to('cpu'), ax, color = '#002635', layer='conv2')
+                    plot_filter_norm(model0.to('cpu'), ax, color = '#AB1A25', layer='conv2')
+
+                    ax.set_ylabel('Max-norm of each filter channel')
+                    ax.set_xlabel('Conv2 output channel')
+                    ax.set_ylim(0,)
+
+                    p1 = mpatches.Patch(color='#002635', label='ConvLassoNet')
+                    p2 = mpatches.Patch(color='#AB1A25', label='unconstrained')
+                    ax.legend(handles=[p1,p2])
+
+                    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv2_filter_norm.png', dpi = 400)
+
+                    fig, axs = plt.subplots(4,4)
+                    plot_filter1(model.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v_, vmax=v_)
+                    fig.suptitle('Filter1 weights ConvLassoNet')
+                    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter1.png', dpi = 400)
+
+                    fig, axs = plt.subplots(4,8)
+                    plot_filter2(model.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v2_, vmax=v2_)
+                    fig.suptitle('Filter2 weights ConvLassoNet')
+                    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter2.png', dpi = 400)
+                    
+                    # continue training with previous model
+                    model_ = deepcopy(model)
+
+                    pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in loss_plots.items() ])).to_csv("losses.csv")
+                    pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in acc_plots.items() ])).to_csv("accuracies.csv")
+                    pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in auroc_plots.items() ])).to_csv("aurocs.csv")
+
+                    show_layer(model.to(dev), 0, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=16)
+                    show_layer(model.to(dev), 3, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=32)
+                    show_layer(model.to(dev), 6, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=64)
+                    show_layer(model.to(dev), 9, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=64)
+                except Exception as e:
+                    print("broken inner inner inner loop")
+                    print(M, l1, e)
+                    continue
     
-    print(model_)
-    show_layer(model_, 0, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=16)
-    show_layer(model_, 3, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=32)
-    show_layer(model_, 6, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=64)
-    show_layer(model_, 9, (1, 128, 128), save_path=f'{plot_dir}/random/', filter_start=0, filter_end=64)
-
-    # print(model_)
-    # test forward method, reshape input to vector with view
-    # model_.forward(images).size()
-    # params of G are already included in params of model!
-    for param in model_.parameters():
-        print(param.size())
-
-
-
-    loss_plots, acc_plots, auroc_plots = {}, {}, {}
-
-    ### Unconstrained "init"
-    opt_ = torch.optim.SGD(model_.parameters(), lr=alpha0, momentum=momentum, nesterov=True)
-    sched_ = StepLR(opt_, step_size=1, gamma=0.9)
-    # record first n_epochs of ramp up
-    hist_ = train_model(model_, opt_, loss, lr_schedule=sched_, n_epochs=n_epochs, device=dev)
-    model0 = deepcopy(model_)
-    try:
-        os.makedirs(f'{plot_dir}/init')
-    except:
-        pass
-    
-
-    show_layer(model_, 0, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=16)
-    show_layer(model_, 3, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=32)
-    show_layer(model_, 6, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=64)
-    show_layer(model_, 9, (1, 128, 128), save_path=f'{plot_dir}/init/', filter_start=0, filter_end=64)
-
-
-    loss_plots[0.]   = hist_['valid_loss']
-    acc_plots[0.]    = hist_['valid_acc']
-    auroc_plots[0.]  = hist_['valid_auroc']
-
-
-
-    model = ConvLassoNet(lambda_=l1, M=M, D_in=(28, 28), D_out=1, kernel_size=3, padding=0, final='softmax', sparse_layer=sparse_me).to(dev)
-
-    opt = torch.optim.SGD(model.parameters(), lr=alpha0, momentum=momentum, nesterov=True)
-    sched = StepLR(opt, step_size=1, gamma=0.9)
-    
-    hist = train_model(model, opt, loss, lr_schedule=sched, n_epochs=n_epochs, pretrained=model_, device=dev)
-    
-    loss_plots[l1]  = hist_['valid_loss']
-    acc_plots[l1]   = hist_['valid_acc']
-    auroc_plots[l1] = hist_['valid_auroc']
-    
-
-    v_, v2_ = 1., 0.1
-    """ Let the Plotting Begin !"""
-    try:
-        os.makedirs(f'{plot_dir}/{M:.1f}_{l1:.2f}')
-        print(f"plotting {plot_dir}, {M:.1f}, {l1:.2f}")
-    except:
-        pass
-
-    ### unconstrained weights
-    fig, axs = plt.subplots(4,4)
-    plot_filter1(model0.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v_, vmax=v_)
-    fig.suptitle('Filter1 weights unconstrained')
-    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter1_unc.png', dpi = 400)
-
-    ### unconstrained weights
-    fig, axs = plt.subplots(4,8)
-    plot_filter2(model0.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v2_, vmax=v2_)
-    fig.suptitle('Filter2 weights unconstrained')
-    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter2_unc.png', dpi = 400)
-
-    # Loss plot
-    fig, axs = plt.subplots(1,3, figsize=(20, 8))
-
-    ax = axs[0]
-    ax.plot(hist['train_loss'], c = '#002635', marker = 'o', label = 'Train loss ConvLassoNet')
-    ax.plot(hist['valid_loss'], c = '#002635', marker = 'x', ls = '--', label = 'Validation loss ConvLassoNet')
-
-    ax.plot(hist_['train_loss'], c = '#AB1A25', marker = 'o', label = 'Train loss unconstrained')
-    ax.plot(hist_['valid_loss'], c = '#AB1A25', marker = 'x', ls = '--', label = 'Validation loss unconstrained')
-
-    ax.set_xlabel('Epoch')
-    ax.set_ylim(0,)
-    ax.legend()
-
-    ax = axs[1]
-    ax.plot(hist['train_acc'], c = '#002635', marker = 'o', label = 'Train accuracy ConvLassoNet', alpha=0.7, markersize=4.)
-    ax.plot(hist['valid_acc'], c = '#002635', marker = 'x', ls = '--', label = 'Validation accuracy ConvLassoNet', markersize=4.)
-    
-    ax.plot(hist_['train_acc'], c = '#AB1A25', marker = 'o', label = 'Train accuracy unconstrained', alpha=0.7, markersize=2.)
-    ax.plot(hist_['valid_acc'], c = '#AB1A25', marker = 'x', ls = '--', label = 'Validation accuracy unconstrained', markersize=2.)
-
-    ax.set_xlabel('Epoch')
-    ax.set_ylim(0.,1.)
-    ax.legend()
-
-    ax = axs[2]
-    ax.plot(hist['train_auroc'], c = '#002635', marker = 'o', label = 'Train AUROC ConvLassoNet', alpha=0.7, markersize=4.)
-    ax.plot(hist['valid_auroc'], c = '#002635', marker = 'x', ls = '--', label = 'Validation AUROC ConvLassoNet', markersize=4.)
-    
-    ax.plot(hist_['train_auroc'], c = '#AB1A25', marker = 'o', label = 'Train AUROC unconstrained', alpha=0.7, markersize=2.)
-    ax.plot(hist_['valid_auroc'], c = '#AB1A25', marker = 'x', ls = '--', label = 'Validation AUROC unconstrained', markersize=2.)
-
-    ax.set_xlabel('Epoch')
-    ax.set_ylim(0.,1.)
-    ax.legend()
-
-    fig.suptitle('Loss with and without LassoNet constraint')
-
-    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_loss.png', dpi = 400)
-
-    fig, ax = plt.subplots()
-    plot_filter_norm(model.to('cpu'), ax, color = '#002635')
-    plot_filter_norm(model0.to('cpu'), ax, color = '#AB1A25')
-
-    ax.set_ylabel('Max-norm of each filter channel')
-    ax.set_xlabel('Conv1 output channel')
-    ax.set_ylim(0,)
-
-    p1 = mpatches.Patch(color='#002635', label='ConvLassoNet')
-    p2 = mpatches.Patch(color='#AB1A25', label='unconstrained')
-    ax.legend(handles=[p1,p2])
-
-    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv1_filter_norm.png', dpi = 400)
-
-    fig, ax = plt.subplots()
-    plot_filter_norm(model.to('cpu'), ax, color = '#002635', layer='conv2')
-    plot_filter_norm(model0.to('cpu'), ax, color = '#AB1A25', layer='conv2')
-
-    ax.set_ylabel('Max-norm of each filter channel')
-    ax.set_xlabel('Conv2 output channel')
-    ax.set_ylim(0,)
-
-    p1 = mpatches.Patch(color='#002635', label='ConvLassoNet')
-    p2 = mpatches.Patch(color='#AB1A25', label='unconstrained')
-    ax.legend(handles=[p1,p2])
-
-    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv2_filter_norm.png', dpi = 400)
-
-    fig, axs = plt.subplots(4,4)
-    plot_filter1(model.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v_, vmax=v_)
-    fig.suptitle('Filter1 weights ConvLassoNet')
-    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter1.png', dpi = 400)
-
-    fig, axs = plt.subplots(4,8)
-    plot_filter2(model.to('cpu'), cmap=plt.cm.RdBu_r, vmin=-v2_, vmax=v2_)
-    fig.suptitle('Filter2 weights ConvLassoNet')
-    fig.savefig(f'{plot_dir}/{M:.1f}_{l1:.2f}/conv_filter2.png', dpi = 400)
-    
-    # continue training with previous model
-    model_ = deepcopy(model)
-
-    pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in loss_plots.items() ])).to_csv("losses.csv")
-    pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in acc_plots.items() ])).to_csv("accuracies.csv")
-    pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in auroc_plots.items() ])).to_csv("aurocs.csv")
-
-    show_layer(model.to(dev), 0, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=16)
-    show_layer(model.to(dev), 3, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=32)
-    show_layer(model.to(dev), 6, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=64)
-    show_layer(model.to(dev), 9, (1, 128, 128), save_path=f'{plot_dir}/{M:.1f}_{l1:.2f}/', filter_start=0, filter_end=64)
-        # except Exception as e:
-        #     print("broken inner inner inner loop")
-        #     print(M, l1, e)
-        #     continue
-    
-        # except Exception as e:
-        #     print("broken somewhere something")
-        #     print(M, l1)
-        #     print(e)
-        #     continue
+        except Exception as e:
+            print("broken somewhere something")
+            print(M, l1)
+            print(e)
+            continue
             
     # except Exception as e:
     #     print("we broke", e)
